a58e70d2828b5c30f0f9de31aef71ba9b2de9d62
matthaus07TW <matthaus.brandao@thoughtworks.com>
2021-07-06 18:07:00 -0300

    chore: Adjustment on nomenclature

--- src/components/Button/ButtonGeneric.jsx
+++ src/components/Button/ButtonGeneric.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Button} from '@material-ui/core';
 
 
-function ButonGeneric({onclick, className, disabled, color, text,variant}) {
+function ButonGeneric({onClick, className, disabled, color, text,variant}) {
     return(
         <Button
             className={className}
             variant={variant}
             color={color}
             disabled={disabled}
-            onClick={onclick}
+            onClick={onClick}
             role="button"
         >
             {text}
