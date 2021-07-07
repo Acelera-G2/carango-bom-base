6dad1f062c3196df5d54cf6d551d7ae883575f7c
matthaus07TW <matthaus.brandao@thoughtworks.com>
2021-07-07 16:59:46 -0300

    Feat: Create module function delete item

--- src/utils/modules.js
+++ src/utils/modules.js
@@ -1,14 +1,24 @@
 export const CrudModule = () =>{
 
-    const addItemsGenerif = (nameGetItem, itemBrand) =>{
+    const addItemGeneric = (nameGetItem, itemBrand) =>{
         let existItem = localStorage.getItem(nameGetItem);
         existItem = existItem ? existItem.split(',') : [];        
         existItem.push(itemBrand);
-
        return localStorage.setItem(nameGetItem,existItem.toString());
     }
 
+    const deleteItemGeneric = (nameGetItem, itemBrand, arrIndexItems) =>{
+        let item = localStorage.getItem(nameGetItem);
+        item = itemBrand.split(',');
+        item = item.filter(function(value, index) {
+            return arrIndexItems.indexOf(index) == -1;
+       })
+       console.log(item)
+       return localStorage.setItem(nameGetItem,item.toString());
+    }
+
     return {
-        add: addItemsGenerif
+        add: addItemGeneric,
+        delete: deleteItemGeneric
     }
 }
