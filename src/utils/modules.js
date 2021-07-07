9233e735b9943f46557a42f80e247c469328a97d
matthaus07TW <matthaus.brandao@thoughtworks.com>
2021-07-07 18:45:53 -0300

    refact: Refatoring Method module crud

--- src/utils/modules.js
+++ src/utils/modules.js
@@ -1,34 +1,30 @@
-export const CrudModule = () =>{
-
-    const addItemGeneric = (nameGetItem, itemArr) =>{
-        let existItem = localStorage.getItem(nameGetItem);
-        existItem = existItem ? existItem.split(',') : [];        
-        existItem.push(itemArr);
-       return localStorage.setItem(nameGetItem,existItem.toString());
+export const CrudModule = (localStorageItem) =>{
+    let getLocalStorage = localStorage.getItem(localStorageItem);
+   
+    const addItemGeneric = (itemArr) =>{
+        getLocalStorage = getLocalStorage ? getLocalStorage.split(',') : [];        
+        getLocalStorage.push(itemArr);
+       return localStorage.setItem(localStorageItem,getLocalStorage.toString());
     }
-
-    const deleteItemGeneric = (nameGetItem, itemArr, arrIndexItems) =>{
-        let item = localStorage.getItem(nameGetItem);
-        item = itemArr.split(',');
-        item = item.filter(function(_, index) {
+    
+    const deleteItemGeneric = ( itemArr, arrIndexItems) =>{
+        getLocalStorage = itemArr.split(',');
+        getLocalStorage = getLocalStorage.filter(function(_, index) {
             return arrIndexItems.indexOf(index) === -1;
        })
-       return localStorage.setItem(nameGetItem,item.toString());
+       return localStorage.setItem(localStorageItem,getLocalStorage.toString());
     }
 
-    const getItemGeneric = (nameGetItem, itemIndex) => {
-        let item = localStorage.getItem(nameGetItem);
-        item = item.split(',').filter((_, index) => index === parseInt(itemIndex))
-        return item[0]
+    const getItemGeneric = (itemIndex) => {
+        getLocalStorage = getLocalStorage.split(',').filter((_, index) => index === parseInt(itemIndex))
+        return getLocalStorage[0]
     }
 
-    const editItemGeneric = (nameGetItem, itemIndex, valueGeneric) => {
-        let item = localStorage.getItem(nameGetItem);
-        item = item.split(',').map((itemGeneric, index) => {
+    const editItemGeneric = (itemIndex, valueGeneric) => {
+        getLocalStorage = getLocalStorage.split(',').map((itemGeneric, index) => {
           return index === parseInt(itemIndex) ? valueGeneric : itemGeneric
         })
-        return localStorage.setItem(nameGetItem,item.toString());
-
+        return localStorage.setItem(localStorageItem,getLocalStorage.toString());
     }
     return {
         add: addItemGeneric,
