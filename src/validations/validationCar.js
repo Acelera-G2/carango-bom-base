f6ee6d14acd1800f7bec5bb7edd39b02f0df3f73
matthaus07TW <matthaus.brandao@thoughtworks.com>
2021-07-14 18:46:04 -0300

    feat: create validate form Car

--- src/validations/validationCar.js
+++ src/validations/validationCar.js
@@ -1,8 +1,27 @@
-export default function validateFormCar(values) {
+function validateFormBrand(values) {
     let errors = {};
-    if (!values.marca) {
-       errors.marca = 'Campo obrigatório';
+    if (values.marca.length < 3) {
+       errors.marca = 'Tem que ser maior que 3 caracteres';
     }
     return errors
+}
+
+function validateFormCar(values) {
+    let errors = {};
+    if (values.moduleCar.length < 3) {
+       errors.moduleCar = 'Tem que ser maior que 3 caracteres';
+    }
+    if (values.year.length < 3) {
+        errors.year = 'Tem que ser maior que 3 caracteres';
+    }
+    if (values.price.length < 3) {
+    errors.price = 'Tem que ser maior que 3 caracteres';
+    }
+    return errors
+}
+
+export  {
+    validateFormBrand,
+    validateFormCar,
 }
     
