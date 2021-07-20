9efc53af1ffbf520cb93fe3c0f22963526a0305c
yankasantosTW <yanka.santos@thoughtworks.com>
2021-07-20 12:08:59 -0300

    chore: add unit test on button component

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react'
-import ButonGeneric from './ButtonGeneric';
+import { render, screen} from '@testing-library/react'
+import { ButtonGeneric } from './ButtonGeneric'
 
 describe('Button', () => {
     it('should have a component button', () => {
         //Assert
-        render(<ButonGeneric/>)
-    
+        render(<ButtonGeneric />);
         //Action
         const buttonComponent = screen.getByRole('button')
-
         //Expect
         expect(buttonComponent).toBeInTheDocument()
     });
