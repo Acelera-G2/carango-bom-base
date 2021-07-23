f2be6a7f3b33458d628196ab0e1c516719845fcb
matthaus07TW <matthaus.brandao@thoughtworks.com>
2021-07-23 18:58:17 -0300

    refact: verify is token exist

--- src/router/Route.jsx
+++ src/router/Route.jsx
@@ -1,60 +1,12 @@
 import React from 'react';
-import { Switch, BrowserRouter } from 'react-router-dom';
-import RouteDefault from './RouteDefault';
-import RegisterBrand from '../pages/Brand/RegisterBrand';
-import Brandlist from '../pages/Brand/BrandList';
-import RegisterCar from '../pages/Car/RegisterCar';
-import RegisterUser from '../pages/User/RegisterUser';
-import ListCar from '../pages/Car/ListCar';
-import ListUser from '../pages/User/ListUser';
-import Login from '../pages/Login/Login';
-
+import { useAuth } from '../hooks/AuthContext';
+import PrivateRoute from './PrivateRouter';
+import PublicRouter from './PublicRouter';
 function Routes() {
+    const { token } = useAuth();
+
     return(
-        <BrowserRouter>    
-            <Switch>
-                <RouteDefault 
-                    path="/register-brand"
-                    component={RegisterBrand}
-                />
-                <RouteDefault 
-                    path='/change-brand/:id'
-                    component={RegisterBrand}
-                />
-                <RouteDefault 
-                    path='/change-car/:id'
-                    component={RegisterCar}
-                />
-                <RouteDefault 
-                    path='/register-car'
-                    component={RegisterCar}
-                />
-                <RouteDefault 
-                    path='/list-car'
-                    component={ListCar}
-                />
-                <RouteDefault 
-                    path='/list-user'
-                    component={ListUser}
-                />
-                <RouteDefault 
-                    path="/register-user"
-                    component={RegisterUser}
-                />
-                <RouteDefault 
-                    path='/change-user/:id'
-                    component={RegisterUser}
-                />
-                <RouteDefault 
-                    path="/Login"
-                    component={Login}
-                />
-                <RouteDefault 
-                    path="/"
-                    component={Brandlist}
-                />
-            </Switch>
-        </BrowserRouter>
+        token ? <PrivateRoute/>:<PublicRouter />
     );
 }
 export default Routes;
