9de91a2067d995589b35d56b17052f57308763c1
yankasantosTW <yanka.santos@thoughtworks.com>
2021-07-23 18:13:50 -0300

    fix: rename file

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from './Button/ButtonGeneric';
+export * from './Input/InputGeneric';
+export * from './Layout/Layout';
+export * from './Navbar/Navbar';
+export * from './TableGrid/TableGrid';
