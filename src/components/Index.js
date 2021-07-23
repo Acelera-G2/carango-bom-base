75a2b72a5b3f29f48f8ff2866088447dbf49fbe7
matthaus07TW <86969747+matthaus07TW@users.noreply.github.com>
2021-07-23 18:11:37 -0300

    Delete Index.js

--- src/components/Index.js
+++
@@ -1,5 +0,0 @@
-export * from './Button/ButtonGeneric';
-export * from './Input/InputGeneric';
-export * from './Layout/Layout';
-export * from './Navbar/Navbar';
-export * from './TableGrid/TableGrid';
